9995d5a604a4acc1b2966fd6841c6180f62f5291
=abhishektanwar <abhishektanwar084@gmail.com>
2021-04-06 15:22:37 +0530

    add readme

--- app.js
+++ app.js
@@ -15,51 +15,13 @@ function errorHandler(error) {
 }
 
 btnTranslate.addEventListener("click",function clickHandler(){
-    var inputText = inputTxt.value; // taking input
-	// console.log(inputText)
-	
-    // calling server for processing
+    var inputText = inputTxt.value;
+
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; // output
+            outputDiv.innerText = translatedText;
         })
         .catch(errorHandler)
 });
-
-
-// var btnTranslate = document.querySelector("#btn-translate");
-// var txtInput = document.querySelector("#input-textarea");
-// var outputDiv = document.querySelector("#output-area");
-
-// // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
-// var serverURL = "https://api.funtranslations.com/translate/minion.json"
-
-
-// function getTranslationURL(input) {
-//     return serverURL + "?" + "text=" + input
-// }
-
-// function errorHandler(error) {
-//     console.log("error occured", error);
-//     alert("something wrong with server! try again after some time")
-// }
-
-
-// function clickHandler() {
-//     var inputText = txtInput.value; // taking input
-// 	// console.log(inputText)
-	
-//     // calling server for processing
-//     fetch(getTranslationURL(inputText))
-//         .then(response => response.json())
-//         .then(json => {
-//             var translatedText = json.contents.translated;
-//             outputDiv.innerText = translatedText; // output
-//         })
-//         .catch(errorHandler)
-// };
-
-// btnTranslate.addEventListener("click", clickHandler)
---
+++ readme.md
@@ -0,0 +1,4 @@
+## minionese
+
+Minionese is a web app that translates the input message into minion language
+[Live Project](https://miniontranslator-abhishek.netlify.app/)
